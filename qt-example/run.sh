f68e0c5ff12a3fdb1f5ed3ddfa126e706ef4c395
cnpatric <82205245+cnpatric@users.noreply.github.com>
2024-12-22 11:08:25 +0100

    test for QT

--- qt-example/run.sh
+++ qt-example/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Change to the script's directory
-cd "$(dirname "$0")"
+# Clean up any existing X locks
+sudo rm -f /tmp/.X0-lock
 
-# Allow file reading for XMLHttpRequest
-export QML_XHR_ALLOW_FILE_READ=1
+# Set environment variables for Qt
+export QT_QPA_PLATFORM=xcb
+export QT_QPA_EGLFS_ALWAYS_SET_MODE=1
+export QT_FONT_DPI=96
+export XDG_RUNTIME_DIR=/tmp/runtime-pi
+
+# Create runtime directory if it doesn't exist
+mkdir -p /tmp/runtime-pi
+chmod 700 /tmp/runtime-pi
 
 # Run the application
-QML_IMPORT_PATH="$PWD" /opt/homebrew/opt/qt@5/bin/qmlscene main.qml
+sudo ./KidsPlayer
