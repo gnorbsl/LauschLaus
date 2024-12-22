b360e1d249b3e195d6002ddee7e5ce01fe82ba6f
cnpatric <82205245+cnpatric@users.noreply.github.com>
2024-12-22 11:21:41 +0100

    test for QT

--- qt-example/run.sh
+++ qt-example/run.sh
@@ -3,23 +3,44 @@
 # Clean up any existing X locks
 sudo rm -f /tmp/.X0-lock
 
-# Set environment variables for Qt
-export QT_QPA_PLATFORM=linuxfb
-export QT_QPA_FB_DRM=1
-export QT_QPA_FB_HIDECURSOR=1
-export QT_QPA_FB_TSLIB=1
-export QT_QPA_GENERIC_PLUGINS=evdevtouch:/dev/input/event0
-export QT_FONT_DPI=96
-export XDG_RUNTIME_DIR=/tmp/runtime-pi
-
 # Create runtime directory if it doesn't exist
 mkdir -p /tmp/runtime-pi
 chmod 700 /tmp/runtime-pi
 
-# Make sure we have access to the framebuffer device
-if [ -e /dev/fb0 ]; then
-    sudo chmod 666 /dev/fb0
-fi
+# Common environment variables
+export QT_FONT_DPI=96
+export XDG_RUNTIME_DIR=/tmp/runtime-pi
 
-# Run the application
-sudo -E ./KidsPlayer
+# Try platforms in order: eglfs -> linuxfb -> minimal
+platforms=("eglfs" "linuxfb" "minimal")
+for platform in "${platforms[@]}"; do
+    echo "Trying platform: $platform"
+    export QT_QPA_PLATFORM=$platform
+    
+    case $platform in
+        "eglfs")
+            export QT_QPA_EGLFS_ALWAYS_SET_MODE=1
+            ;;
+        "linuxfb")
+            export QT_QPA_FB_DRM=1
+            export QT_QPA_FB_HIDECURSOR=1
+            export QT_QPA_FB_TSLIB=1
+            if [ -e /dev/fb0 ]; then
+                sudo chmod 666 /dev/fb0
+            fi
+            ;;
+        "minimal")
+            unset QT_QPA_FB_DRM
+            unset QT_QPA_FB_HIDECURSOR
+            unset QT_QPA_FB_TSLIB
+            ;;
+    esac
+    
+    # Try to run the application
+    sudo -E ./KidsPlayer
+    
+    # If the application exits with status 0, break the loop
+    if [ $? -eq 0 ]; then
+        break
+    fi
+done
